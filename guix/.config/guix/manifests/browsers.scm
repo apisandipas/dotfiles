9466c19022a0f7a94b3c0b70b1abd24e5b7d16ff
Bryan Paronto <bryan@cablecar.digtal>
2022-04-10 18:54:46 -0500

    add manifests back

---
+++ guix/.config/guix/manifests/browsers.scm
@@ -0,0 +1,12 @@
+;; This file contains the list of browsers I use occasionally so that I don't
+;; need to install their updates when unnecessary.
+
+(specifications->manifest
+ '(
+   ;; TODO: Add qutebrowser config
+   "qutebrowser"
+   ;; TODO: add Nyxt config
+   "nyxt"
+   "firefox"
+   "ungoogled-chromium"
+   ))
