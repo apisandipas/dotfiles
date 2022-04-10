bebe26d93c1519b15a50976ea5092c8326e5daf0
Bryan Paronto <bryan@cablecar.digtal>
2022-04-10 18:36:04 -0500

    added tassos guix channel

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -17,7 +17,12 @@
        (name 'nonguix)
        (url "https://gitlab.com/nonguix/nonguix")
        (branch "master"))
+      ;; (channel
+      ;;  (name 'guixed)
+      ;;  (url "https://github.com/apisandipas/guixed")
+      ;;  (branch "main"))
       (channel
-       (name 'guixed)
-       (url "https://github.com/apisandipas/guixed")
-       (branch "main")))
+       (name 'tassos)
+       (url "https://github.com/Tass0sm/tassos-guix")
+       (branch "main"))
+      )
