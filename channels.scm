9516fc0a45af1e6aadbd40e6f8f7a6e08f4b2108
Bryan Paronto <bryan@cablecar.digtal>
2022-04-19 16:27:45 -0500

    remvoed extra guix channels for testing

--- channels.scm
+++ channels.scm
@@ -2,18 +2,18 @@
 (list (channel
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git"))
-      (channel
-       (name 'channel-x)
-       (url "https://github.com/daviwil/channel-x"))
-      (channel
-       (name 'rekahsoft)
-       (url "https://git.rekahsoft.ca/rekahsoft/rekahsoft-guix"))
+      ;; (channel
+      ;;  (name 'channel-x)
+      ;;  (url "https://github.com/daviwil/channel-x"))
+      ;; (channel
+      ;;  (name 'rekahsoft)
+      ;;  (url "https://git.rekahsoft.ca/rekahsoft/rekahsoft-guix"))
       (channel
        (name 'flat)
        (url "https://github.com/flatwhatson/guix-channel.git"))
-      (channel
-       (name 'rde)
-       (url "https://git.sr.ht/~abcdw/rde"))
+      ;; (channel
+      ;;  (name 'rde)
+      ;;  (url "https://git.sr.ht/~abcdw/rde"))
       (channel
        (name 'nonguix)
        (url "https://gitlab.com/nonguix/nonguix")
