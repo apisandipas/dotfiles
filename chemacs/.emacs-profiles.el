24b89ad44a218007eaaf5a0f83471a0d0b236bfd
Bryan Paronto <bryan@kindsys.us>
2023-04-05 13:08:15 -0500

    updates

--- chemacs/.emacs-profiles.el
+++ chemacs/.emacs-profiles.el
@@ -4,5 +4,9 @@
 
  ("default" . ((user-emacs-directory . "~/.emacs.doom")
                (server-name . "default")))
+ ("nano" . ((user-emacs-directory . "~/.emacs.nano")
+               (server-name . "nano")))
+ ("vanilla" . ((user-emacs-directory . "~/.emacs.vanilla")
+               (server-name . "vanilla")))
 
  )
