899cac05ae4ddb510a3a845be86278287db5a0eb
Bryan Paronto <bryan@kindsys.us>
2024-01-28 13:46:51 -0600

    merge conflict in keybinds.zsh

--- zsh/.config/zsh/lib/keybinds.zsh
+++ zsh/.config/zsh/lib/keybinds.zsh
@@ -1,11 +1,6 @@
 #!/usr/bin/env zsh
 
-<<<<<<< HEAD
 # Disabled CapsLock
 if [ "$XDG_SESSION_TYPE" = "x11" ]; then
   setxkbmap -option ctrl:nocaps
-=======
-if [ "$XDG_SESSION_TYPE" = "x11" ]; then
-  setxkbmap -option ctrl:nocaps;
->>>>>>> 08ff1b3 (updates to galactus)
 fi
